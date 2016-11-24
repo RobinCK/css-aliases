66256affb98d23bda060ac940a28bacebb686ffd
Igor Ognichenko <ognichenko.igor@gmail.com>
2016-11-24 10:15:22 +0200

    bug fixes

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var path = require('path');
-var rework = require('rework');
-var url = require('rework-plugin-url');
-var reworkCss = require('css');
+var namedRegexp = require("named-js-regexp");
 
 module.exports = function (css, resourcePath, aliases) {
-    var obj = reworkCss.parse(css);
-    var resolve = function(url, resourcePath) {
+    var IMPORT_REGEX = namedRegexp(/^(:<import>@import\s+)(:<url_tag_open>url\()?\s*(:<quote_open>"|')(:<url>.+)\s*(:<quote_close>"|')(:<url_tag_close>\))?(:<context>.*);$/gm);
+    var URL_REGEX = namedRegexp(/(:<url_tag_open>url\()\s*(:<quote_open>"|')?(:<url>.+)\s*(:<quote_close>"|')?(:<url_tag_close>\))/gm);
+    css = parse(css, IMPORT_REGEX);
+    css = parse(css, URL_REGEX);
+
+    return css;
+
+    function resolve(url, resourcePath) {
         for (var aliasName in aliases) {
             var regExp = new RegExp('^' + aliasName + '(.*)')
 
@@ -19,18 +23,33 @@ module.exports = function (css, resourcePath, aliases) {
         return url;
     }
 
-    for (var style in obj.stylesheet.rules) {
-        if (obj.stylesheet.rules[style].type === 'import') {
-            obj.stylesheet.rules[style].import = '"' + resolve(obj.stylesheet.rules[style].import.toString().replace (/(^"|')(.+?)("|'$)/, '$2'), resourcePath, aliases) + '"';
+    function parse(css, regex) {
+        var m, importOriginal, importReplace;
+
+        while ((m = regex.exec(css)) !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            importOriginal = '';
+            importReplace = '';
+            var mathGroups = m.groups();
+            for (var groupName in mathGroups) {
+                var match = mathGroups[groupName];
+                importOriginal += typeof match !== 'undefined' ? match : '';
+
+                if (groupName !== 'url') {
+                    importReplace += typeof match !== 'undefined' ? match : '';
+                } else {
+                    importReplace += resolve(match, '.');
+                }
+            }
+
+            if (importOriginal !== importReplace) {
+                css = css.replace(importOriginal, importReplace);
+            }
         }
+
+        return css;
     }
-
-    css = reworkCss.stringify(obj);
-
-    return rework(css)
-        .use(url(function(url) {
-            return resolve(url, resourcePath, aliases);
-        }))
-        .toString()
-    ;
 }
